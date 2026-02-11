2eaae26c6a6d5b1f3ead2b81b926d54689eb6b2e
RogueScr1be <prentiss@fromtheculture.co>
2026-02-10 20:36:26 -0600

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
-
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // Keep expo-router plugin (warning is fine; not your blocker)
-      'expo-router/babel',
-
-      // IMPORTANT:
-      // '@' must point to project root, NOT './app'
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './',
-          },
-          extensions: [
-            '.ios.js',
-            '.android.js',
-            '.js',
-            '.ts',
-            '.tsx',
-            '.json',
-          ],
-        },
-      ],
-    ],
   };
 };
